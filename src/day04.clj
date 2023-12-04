83f0ba88b8daa5181ffb62ad2e00518f6aba4006
Espen Amble Kolstad <espenamblekolstad@gmail.com>
2023-12-04 07:11:10 +0100

    Day 4

---
+++ src/day04.clj
@@ -0,0 +1,49 @@
+(ns day04
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
+
+(def input (slurp (str "resources/" *ns* ".txt")))
+(def small-input "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+
+(defn parse-card [l]
+  (->> (str/split l #"\s*[:+\|]\s*")
+       next
+       (mapv #(into #{} (map parse-long) (str/split % #"\s+")))))
+
+(defn count-winning [[win-cards cards]]
+  (count (set/intersection win-cards cards)))
+
+(comment
+
+  (->> (str/split-lines #_small-input input)
+       (transduce (comp
+                    (map parse-card)
+                    (map count-winning)
+                    (filter pos?)
+                    (map dec)
+                    (map #(Math/round (Math/pow 2 %))))
+                  +))
+  ;; 21105
+
+  (let [cs (->> (str/split-lines #_small-input input)
+                (into {}
+                      (comp (map parse-card)
+                            (map count-winning)
+                            (map-indexed vector)
+                            (map #(-> %
+                                      (update 0 inc)
+                                      (update 1 (partial vector 1)))))))
+        max-end (inc (apply max (keys cs)))]
+    (->> (range 1 max-end)
+         (reduce (fn [cs i]
+                   (let [[cnt len] (get cs i)]
+                     (reduce (fn [cs i] (update cs i update 0 + cnt))
+                             cs
+                             (range (inc i) (min max-end (+ len (inc i)))))))
+                 cs)
+
+         vals
+         (map first)
+         (reduce +)))
+  ;; 5329815
+  )
